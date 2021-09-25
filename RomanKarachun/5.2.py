fea34dd68de08084561d94c3b8a9e80c5bf3f3d8
RomanKarachun <87867142+RomanKarachun@users.noreply.github.com>
2021-09-25 18:15:38 +0300

    Delete 5.2.py

--- RomanKarachun/5.2.py
+++
@@ -1,8 +0,0 @@
-import re
-import collections
-with open("lorem_ipsum.txt") as f:
-    text = f.read()
-
-words = re.compile(r"[\w']+", re.U).findall(text)
-counts = collections.Counter(words).most_common(3)
-print(counts)
