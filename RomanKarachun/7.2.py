11cda6cb5836a24afed6e92b4baeb37407600d55
RomanKarachun <87867142+RomanKarachun@users.noreply.github.com>
2021-10-10 20:49:07 +0300

    Delete 7.2.py

--- RomanKarachun/7.2.py
+++
@@ -1,9 +0,0 @@
-from contextlib import contextmanager
-
-@contextmanager
-def open_file(name):
-    f = open(name, "w")
-    yield f
-    f.close()
-with open_file('test72.txt') as f:
-    f.write('Python has released version 3.10')
